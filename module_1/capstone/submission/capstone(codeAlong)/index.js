89f49f16d6257fac8ca61e7ef9ee268d0f85bf11
zulfidly <114971952+zulfidly@users.noreply.github.com>
2023-01-16 17:13:14 +0800

    code-along-version

---
+++ module_1/capstone/submission/capstone(codeAlong)/index.js
@@ -0,0 +1,15 @@
+console.log(`
+
+npm i passport (authentication)
+
+npm i date-fns (date manipulation / formatting)
+
+npm i vuetify (UI components)
+
+npm i -D tailwindcss (styling)
+
+npm i axios (API calls)
+
+npm i dotenv (environment variables)
+
+`)
